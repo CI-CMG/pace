e271ae5ecc52740dff8f90e62140904c1bb21184
paytoncain <payton.cain@colorado.edu>
2024-04-05 00:39:25 -0600

    Generate python classes within build

---
+++ pace-data/src/main/resources/python/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pacedata',
+    version='${python.project.version}',
+    packages=find_packages(),
+    description='Passive Acoustic Collection Engine Data',
+    author='Center for Integrated Research in Environmental Sciences (CIRES)',
+    url='https://github.com/CI-CMG/pace/tree/trunk/pace-data',
+)
