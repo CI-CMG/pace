f98aca126d0c2fe570ce7848c6ff55ff6476433a
paytoncain <payton.cain@colorado.edu>
2024-05-24 14:20:27 -0600

    Create user properties file in .pace directory

--- pace-utilities/src/main/java/edu/colorado/cires/pace/utilities/ApplicationPropertyResolver.java
+++ pace-utilities/src/main/java/edu/colorado/cires/pace/utilities/ApplicationPropertyResolver.java
@@ -1,21 +1,32 @@
 package edu.colorado.cires.pace.utilities;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Properties;
 import java.util.function.Function;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.StringUtils;
 
 public final class ApplicationPropertyResolver {
 
-  private static final String APPLICATION_BASE_DIR = ".pace";
+  private static final Path APPLICATION_BASE_DIR = Paths.get(System.getProperty("user.home")).resolve(".pace");
 
   public <T> T getPropertyValue(String propertyName, Function<String, T> transform) {
     Properties properties = new Properties();
-    try (InputStream inputStream = getClass().getResourceAsStream("/application.properties")) {
-      properties.load(inputStream);
+    try {
+      createPropertiesFileAndConfigDir();
+
+      try (InputStream defaultInputStream = getClass().getResourceAsStream("/application.properties");
+          InputStream userPropertiesInputStream = new FileInputStream(getPropertiesFilePath().toFile())) {
+        properties.load(defaultInputStream);
+        properties.load(userPropertiesInputStream);
+      }
     } catch (IOException e) {
       throw new IllegalStateException(String.format(
           "Failed to read %s value from application properties", propertyName
@@ -30,13 +41,42 @@ public final class ApplicationPropertyResolver {
     
     return transform.apply(propertyValue);
   }
+  
+  private Path getConfigDir() {
+    return APPLICATION_BASE_DIR.resolve("config");
+  }
+  
+  private Path getPropertiesFilePath() {
+    return getConfigDir().resolve("application.properties");
+  }
+  
+  private void createPropertiesFileAndConfigDir() throws IOException {
+    Path propertiesFile = getPropertiesFilePath();
+    if (propertiesFile.toFile().exists()) {
+      return;
+    }
+    
+    if (!propertiesFile.getParent().toFile().exists()) {
+      FileUtils.createParentDirectories(propertiesFile.toFile());
+    }
+
+    try (InputStream inputStream = getClass().getResourceAsStream("/application.properties"); OutputStream outputStream = new FileOutputStream(propertiesFile.toFile())) {
+      if (inputStream == null) {
+        throw new FileNotFoundException("application.properties file not found in application resources");
+      }
+      Properties properties = new Properties();
+      properties.load(inputStream);
+      
+      properties.remove("pace.version");
+      
+      properties.store(outputStream, "PACE user properties");
+    }
+  }
 
   public Path getDataDir() {
     String dirString = getPropertyValue("pace.data-dir", (s) -> s);
     if (StringUtils.isBlank(dirString)) {
-      return Path.of(
-          System.getProperty("user.home")
-      ).resolve(APPLICATION_BASE_DIR).resolve("data").toAbsolutePath();
+      return APPLICATION_BASE_DIR.resolve("data").toAbsolutePath();
     }
 
     return Paths.get(dirString).toAbsolutePath();
