26bf4f53d5ddeaf2ffe7d635dbccc16f544de923
paytoncain <payton.cain@colorado.edu>
2024-06-12 17:59:24 -0600

    Improve migration logging

--- pace-migrator/src/main/java/edu/colorado/cires/pace/migrator/Migrator.java
+++ pace-migrator/src/main/java/edu/colorado/cires/pace/migrator/Migrator.java
@@ -8,18 +8,24 @@ import edu.colorado.cires.pace.repository.NotFoundException;
 import jakarta.validation.ConstraintViolationException;
 import java.util.Objects;
 import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public final class Migrator {
+  
+  private static final Logger LOGGER = LoggerFactory.getLogger(Migrator.class);
   
   public static <O extends ObjectWithUniqueField> void migrate(MigrationRepositoryPair<O> migrationRepositoryPair) throws MigrationException {
     RuntimeException runtimeException;
     try {
+      LOGGER.info("Migrating {} objects", migrationRepositoryPair.getInputRepository().getClassName());
       runtimeException = migrationRepositoryPair.getInputRepository().findAll().map(
               (objectWithUniqueField -> {
                 try {
                   migrationRepositoryPair.getOutputRepository().create(objectWithUniqueField);
                   return null;
                 } catch (DatastoreException | ConflictException | NotFoundException | BadArgumentException e) {
+                  LOGGER.error("Error encountered during {} objects migration: {}", migrationRepositoryPair.getInputRepository().getClassName(), e.getMessage());
                   return new RuntimeException(e);
                 } catch (ConstraintViolationException e) {
                   Throwable cause = new RuntimeException(String.format(
@@ -27,6 +33,7 @@ public final class Migrator {
                               .map(v -> String.format("%s: %s", v.getPropertyPath().toString(), v.getMessage()))
                           .collect(Collectors.joining(", "))
                   ));
+                  LOGGER.error("Error encountered during {} objects migration: {}", migrationRepositoryPair.getInputRepository().getClassName(), e.getMessage());
                   return new RuntimeException(cause);
                 }
               })
@@ -55,6 +62,7 @@ public final class Migrator {
       
       throw migrationException;
     }
+    LOGGER.info("Successfully migrated {} objects", migrationRepositoryPair.getInputRepository().getClassName());
   }
 
 }
