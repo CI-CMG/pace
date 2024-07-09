8548f3d0c4f4e7baf9e81236df3a966cbabed17c
paytoncain <payton.cain@colorado.edu>
2024-07-09 14:43:21 -0600

    Update spreadsheet converter error handling

--- pace-translator/pace-common-translator/src/main/java/edu/colorado/cires/pace/translator/SpreadsheetConverter.java
+++ pace-translator/pace-common-translator/src/main/java/edu/colorado/cires/pace/translator/SpreadsheetConverter.java
@@ -4,30 +4,31 @@ import edu.colorado.cires.pace.data.object.ObjectWithUniqueField;
 import edu.colorado.cires.pace.data.translator.Translator;
 import edu.colorado.cires.pace.translator.TranslatorExecutor.MapWithRowNumber;
 import edu.colorado.cires.pace.translator.converter.Converter;
-import java.util.Objects;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 public class SpreadsheetConverter {
   
-  public static <T extends Translator, O extends ObjectWithUniqueField> Stream<O> execute(Supplier<Stream<MapWithRowNumber>> reader, T translator, Converter<T, O> converter) {
+  public static <T extends Translator, O extends ObjectWithUniqueField> Stream<ObjectWithRowException<O>> execute(Supplier<Stream<MapWithRowNumber>> reader, T translator, Converter<T, O> converter) {
     RuntimeException runtimeException = new RuntimeException("Translation failed");
-    
-    Stream<O> objectStream = reader.get()
+
+    return reader.get()
         .map(mapWithRowNumber -> {
           try {
-            return converter.convert(translator, mapWithRowNumber.map(), mapWithRowNumber.row(), runtimeException);
+            RuntimeException rte = new RuntimeException();
+            O object = converter.convert(translator, mapWithRowNumber.map(), mapWithRowNumber.row(), rte);
+            if (rte.getSuppressed().length > 0) {
+              for (Throwable throwable : rte.getSuppressed()) {
+                runtimeException.addSuppressed(throwable);
+              }
+              return new ObjectWithRowException<>(object, mapWithRowNumber.row(), rte);
+            }
+            return new ObjectWithRowException<>(object, mapWithRowNumber.row(), null);
           } catch (TranslationException e) {
             runtimeException.addSuppressed(e);
-            return null;
+            return new ObjectWithRowException<>(null, mapWithRowNumber.row(), e);
           }
-        }).filter(Objects::nonNull);
-    
-    if (runtimeException.getSuppressed().length > 0) {
-      throw runtimeException;
-    }
-    
-    return objectStream;
+        });
   }
 
 }
