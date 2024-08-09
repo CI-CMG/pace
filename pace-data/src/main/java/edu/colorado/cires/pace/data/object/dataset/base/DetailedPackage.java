14948aaf1b340cadb03a28c924232fef37fca828
paytoncain <payton.cain@colorado.edu>
2024-08-09 11:21:27 -0600

    Remove detailed packages

--- pace-data/src/main/java/edu/colorado/cires/pace/data/object/dataset/base/DetailedPackage.java
+++ pace-data/src/main/java/edu/colorado/cires/pace/data/object/dataset/base/DetailedPackage.java
@@ -1,16 +1,8 @@
 package edu.colorado.cires.pace.data.object.dataset.base;
 
-import com.fasterxml.jackson.annotation.JsonSubTypes;
-import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonTypeInfo.As;
 import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
-import edu.colorado.cires.pace.data.object.dataset.audio.DetailedAudioPackage;
-import edu.colorado.cires.pace.data.object.dataset.audio.DetailedCPODPackage;
-import edu.colorado.cires.pace.data.object.dataset.detections.DetailedDetectionsPackage;
-import edu.colorado.cires.pace.data.object.dataset.soundClips.DetailedSoundClipsPackage;
-import edu.colorado.cires.pace.data.object.dataset.soundLevelMetrics.DetailedSoundLevelMetricsPackage;
-import edu.colorado.cires.pace.data.object.dataset.soundPropagationModels.DetailedSoundPropagationModelsPackage;
 
 import java.util.Collections;
 import java.util.List;
@@ -20,14 +12,6 @@ import lombok.EqualsAndHashCode;
 import lombok.experimental.SuperBuilder;
 
 @JsonTypeInfo(use = Id.NAME, include = As.EXTERNAL_PROPERTY, property = "type")
-@JsonSubTypes({
-    @Type(value = DetailedAudioPackage.class, name = "audio"),
-    @Type(value = DetailedCPODPackage.class, name = "CPOD"),
-    @Type(value = DetailedDetectionsPackage.class, name = "detections"),
-    @Type(value = DetailedSoundClipsPackage.class, name = "sound clips"),
-    @Type(value = DetailedSoundLevelMetricsPackage.class, name = "sound level metrics"),
-    @Type(value = DetailedSoundPropagationModelsPackage.class, name = "sound propagation models"),
-})
 @Data
 @EqualsAndHashCode(callSuper = true)
 @SuperBuilder(toBuilder = true)
