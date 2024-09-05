0d0334a78dee7040176f2fc14d97d86c7cd9b657
Quincy Cantu <quincyscantu@gmail.com>
2024-09-05 15:11:39 -0600

    Checking jar filelist format try 16

--- build-resources/mac-signing.sh
+++ build-resources/mac-signing.sh
@@ -2,9 +2,9 @@ jar -xf ./jars/pace-cli-0.1.1-SNAPSHOT.jar BOOT-INF/lib/*
 
 for filename in /BOOT-INF/lib/*; do
   echo "$filename"
-  if [ -f "$filename" ]; then
-    fileList=$(jar -tf filename | grep .dylib)
-    echo fileList
-  fi
+  #if [ -f "$filename" ]; then
+  fileList=$(jar -tf filename | grep .dylib)
+  echo fileList
+  #fi
 done
 
