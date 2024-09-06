598e7492ef07f23e4de8fd3151ac7dbb4859979a
Chris Slater <6004777+electricsam@users.noreply.github.com>
2024-09-06 10:39:27 -0600

    add quotes to env vars

--- build-resources/mac-signing.sh
+++ build-resources/mac-signing.sh
@@ -1,14 +1,12 @@
 set -ex
 
-security find-certificate -a sign-keychain
-
 echo Extracting dylib files
 jar -xf ./jars/pace-cli-0.1.1-SNAPSHOT.jar org/sqlite/native/Mac/aarch64/libsqlitejdbc.dylib
 jar -xf ./jars/pace-cli-0.1.1-SNAPSHOT.jar org/sqlite/native/Mac/x86_64/libsqlitejdbc.dylib
 
 echo Signing dylib files
-/usr/bin/codesign -s "Developer ID Application: $2" -vvvv --timestamp --options runtime --prefix 'edu.colorado.cires.pace.cli.' --keychain $1 --force org/sqlite/native/Mac/aarch64/libsqlitejdbc.dylib
-/usr/bin/codesign -s "Developer ID Application: $2" -vvvv --timestamp --options runtime --prefix 'edu.colorado.cires.pace.cli.' --keychain $1 --force org/sqlite/native/Mac/x86_64/libsqlitejdbc.dylib
+/usr/bin/codesign -s "Developer ID Application: $2" -vvvv --timestamp --options runtime --prefix 'edu.colorado.cires.pace.cli.' --keychain "$1" --force org/sqlite/native/Mac/aarch64/libsqlitejdbc.dylib
+/usr/bin/codesign -s "Developer ID Application: $2" -vvvv --timestamp --options runtime --prefix 'edu.colorado.cires.pace.cli.' --keychain "$1" --force org/sqlite/native/Mac/x86_64/libsqlitejdbc.dylib
 
 echo Repacking dylib files
 jar -uf ./jars/pace-cli-0.1.1-SNAPSHOT.jar org/sqlite/native/Mac/aarch64/libsqlitejdbc.dylib
