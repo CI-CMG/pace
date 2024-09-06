375beef167f490f41abacd4fd1b6adf037509b9b
Quincy Cantu <quincyscantu@gmail.com>
2024-09-06 08:42:55 -0600

    Sign and repack both dylib files

--- build-resources/mac-signing.sh
+++ build-resources/mac-signing.sh
@@ -1,8 +1,9 @@
 jar -xf ./jars/pace-cli-0.1.1-SNAPSHOT.jar org/sqlite/native/Mac/aarch64/libsqlitejdbc.dylib
 jar -xf ./jars/pace-cli-0.1.1-SNAPSHOT.jar org/sqlite/native/Mac/x86_64/libsqlitejdbc.dylib
-ls org
 
-/usr/bin/codesign -s <signing-identity> -vvvv --timestamp --options runtime --prefix <identifier-prefix> --keychain <keychain> --force <file-path>
+/usr/bin/codesign -s $4 -vvvv --timestamp --options runtime --prefix $3 --keychain $1 --force org/sqlite/native/Mac/aarch64/libsqlitejdbc.dylib
+/usr/bin/codesign -s $4 -vvvv --timestamp --options runtime --prefix $3 --keychain $1 --force org/sqlite/native/Mac/x86_64/libsqlitejdbc.dylib
 
-jar -uf ./jars/pace-cli-0.1.1-SNAPSHOT.jar <path-to-dylib>
-jar -uf ./jars/pace-cli-0.1.1-SNAPSHOT.jar <path-to-dylib>
+
+jar -uf ./jars/pace-cli-0.1.1-SNAPSHOT.jar org/sqlite/native/Mac/aarch64/libsqlitejdbc.dylib
+jar -uf ./jars/pace-cli-0.1.1-SNAPSHOT.jar org/sqlite/native/Mac/x86_64/libsqlitejdbc.dylib
