fc47710e84127f870f761a48faea5764f2e448d1
Quincy Cantu <quincyscantu@gmail.com>
2024-09-06 08:32:56 -0600

    Checking org layout

--- build-resources/mac-signing.sh
+++ build-resources/mac-signing.sh
@@ -1,11 +1,8 @@
 jar -xf ./jars/pace-cli-0.1.1-SNAPSHOT.jar org/sqlite/native/Mac/aarch64/libsqlitejdbc.dylib
 jar -xf ./jars/pace-cli-0.1.1-SNAPSHOT.jar org/sqlite/native/Mac/x86_64/libsqlitejdbc.dylib
-ls
+ls org
 
-for filename in /BOOT-INF/lib/*.jar; do
-  #echo "$filename"
-  if [ -f "$filename" ]; then
-    fileList=$(jar -tf "$filename" | grep .dylib)
-    echo "$fileList"
-  fi
-done
+/usr/bin/codesign -s <signing-identity> -vvvv --timestamp --options runtime --prefix <identifier-prefix> --keychain <keychain> --force <file-path>
+
+jar -uf ./jars/pace-cli-0.1.1-SNAPSHOT.jar <path-to-dylib>
+jar -uf ./jars/pace-cli-0.1.1-SNAPSHOT.jar <path-to-dylib>
