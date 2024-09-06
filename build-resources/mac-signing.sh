cd1d0cabe38164476a9a32858edcce2d7b6cac91
Quincy Cantu <quincyscantu@gmail.com>
2024-09-06 09:13:16 -0600

    Corrected signing identifiers

--- build-resources/mac-signing.sh
+++ build-resources/mac-signing.sh
@@ -1,9 +1,8 @@
 jar -xf ./jars/pace-cli-0.1.1-SNAPSHOT.jar org/sqlite/native/Mac/aarch64/libsqlitejdbc.dylib
 jar -xf ./jars/pace-cli-0.1.1-SNAPSHOT.jar org/sqlite/native/Mac/x86_64/libsqlitejdbc.dylib
 
-/usr/bin/codesign -s $4 -vvvv --timestamp --options runtime --prefix $3 --keychain $1 --force org/sqlite/native/Mac/aarch64/libsqlitejdbc.dylib
-/usr/bin/codesign -s $4 -vvvv --timestamp --options runtime --prefix $3 --keychain $1 --force org/sqlite/native/Mac/x86_64/libsqlitejdbc.dylib
-
+/usr/bin/codesign -s $2 -vvvv --timestamp --options runtime --prefix 'edu.colorado.cires.pace.cli.' --keychain $1 --force org/sqlite/native/Mac/aarch64/libsqlitejdbc.dylib
+/usr/bin/codesign -s $2 -vvvv --timestamp --options runtime --prefix 'edu.colorado.cires.pace.cli.' --keychain $1 --force org/sqlite/native/Mac/x86_64/libsqlitejdbc.dylib
 
 jar -uf ./jars/pace-cli-0.1.1-SNAPSHOT.jar org/sqlite/native/Mac/aarch64/libsqlitejdbc.dylib
 jar -uf ./jars/pace-cli-0.1.1-SNAPSHOT.jar org/sqlite/native/Mac/x86_64/libsqlitejdbc.dylib
