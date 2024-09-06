fc43257668f4a3f21f2ba6a85a45bf32861f75d3
Quincy Cantu <quincyscantu@gmail.com>
2024-09-06 11:38:13 -0600

    create keychain

--- build-resources/mac-signing.sh
+++ build-resources/mac-signing.sh
@@ -5,6 +5,7 @@ jar -xf ./jars/pace-cli-0.1.1-SNAPSHOT.jar org/sqlite/native/Mac/aarch64/libsqli
 jar -xf ./jars/pace-cli-0.1.1-SNAPSHOT.jar org/sqlite/native/Mac/x86_64/libsqlitejdbc.dylib
 
 echo Signing dylib files
+security list-keychains -d user -s "$(security list-keychains -d user | sed -e s/\"//g)" "edu.colorado.cires.pace.cli.$1"
 /usr/bin/codesign -s 'Developer ID Application: University of Colorado Boulder (8JR6566HZ6)' -vvvv --timestamp --options runtime --prefix 'edu.colorado.cires.pace.cli.' --keychain "$1" --force org/sqlite/native/Mac/aarch64/libsqlitejdbc.dylib
 /usr/bin/codesign -s 'Developer ID Application: University of Colorado Boulder (8JR6566HZ6)' -vvvv --timestamp --options runtime --prefix 'edu.colorado.cires.pace.cli.' --keychain "$1" --force org/sqlite/native/Mac/x86_64/libsqlitejdbc.dylib
 
