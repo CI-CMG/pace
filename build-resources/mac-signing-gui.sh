92ccc3965983f9d1d0e04f9b516e515b58d4aed2
Quincy Cantu <quincyscantu@gmail.com>
2024-09-09 09:02:22 -0600

    Uncommented needed lines of signing

--- build-resources/mac-signing-gui.sh
+++ build-resources/mac-signing-gui.sh
@@ -6,10 +6,10 @@ echo Extracting gui dylib files
 jar -xf ./jars/pace-gui-0.1.1-SNAPSHOT.jar com/formdev/flatlaf/natives/libflatlaf-macos-arm64.dylib
 jar -xf ./jars/pace-gui-0.1.1-SNAPSHOT.jar com/formdev/flatlaf/natives/libflatlaf-macos-x86_64.dylib
 
-#echo Signing gui dylib files
-#/usr/bin/codesign -s 'Developer ID Application: University of Colorado Boulder (8JR6566HZ6)' -vvvv --timestamp --options runtime --prefix 'edu.colorado.cires.pace.gui.' --keychain "$1" --force org/sqlite/native/Mac/aarch64/libsqlitejdbc.dylib
-#/usr/bin/codesign -s 'Developer ID Application: University of Colorado Boulder (8JR6566HZ6)' -vvvv --timestamp --options runtime --prefix 'edu.colorado.cires.pace.gui.' --keychain "$1" --force org/sqlite/native/Mac/x86_64/libsqlitejdbc.dylib
-#
-#echo Repacking gui dylib files
-#jar -uf ./jars/pace-gui-0.1.1-SNAPSHOT.jar org/sqlite/native/Mac/aarch64/libsqlitejdbc.dylib
-#jar -uf ./jars/pace-gui-0.1.1-SNAPSHOT.jar org/sqlite/native/Mac/x86_64/libsqlitejdbc.dylib
+echo Signing gui dylib files
+/usr/bin/codesign -s 'Developer ID Application: University of Colorado Boulder (8JR6566HZ6)' -vvvv --timestamp --options runtime --prefix 'edu.colorado.cires.pace.gui.' --keychain "$1" --force com/formdev/flatlaf/natives/libflatlaf-macos-arm64.dylib
+/usr/bin/codesign -s 'Developer ID Application: University of Colorado Boulder (8JR6566HZ6)' -vvvv --timestamp --options runtime --prefix 'edu.colorado.cires.pace.gui.' --keychain "$1" --force com/formdev/flatlaf/natives/libflatlaf-macos-x86_64.dylib
+
+echo Repacking gui dylib files
+jar -uf ./jars/pace-gui-0.1.1-SNAPSHOT.jar com/formdev/flatlaf/natives/libflatlaf-macos-arm64.dylib
+jar -uf ./jars/pace-gui-0.1.1-SNAPSHOT.jar com/formdev/flatlaf/natives/libflatlaf-macos-x86_64.dylib
