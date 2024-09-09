f414f59787f5cc8e4c993055c3020dccb081d4ae
Quincy Cantu <quincyscantu@gmail.com>
2024-09-09 08:24:07 -0600

    Signing added for gui

--- build-resources/mac-signing.sh
+++ build-resources/mac-signing.sh
@@ -1,13 +1,25 @@
 set -ex
 
-echo Extracting dylib files
+echo Extracting cli dylib files
 jar -xf ./jars/pace-cli-0.1.1-SNAPSHOT.jar org/sqlite/native/Mac/aarch64/libsqlitejdbc.dylib
 jar -xf ./jars/pace-cli-0.1.1-SNAPSHOT.jar org/sqlite/native/Mac/x86_64/libsqlitejdbc.dylib
 
-echo Signing dylib files
+echo Signing cli dylib files
 /usr/bin/codesign -s 'Developer ID Application: University of Colorado Boulder (8JR6566HZ6)' -vvvv --timestamp --options runtime --prefix 'edu.colorado.cires.pace.cli.' --keychain "$1" --force org/sqlite/native/Mac/aarch64/libsqlitejdbc.dylib
 /usr/bin/codesign -s 'Developer ID Application: University of Colorado Boulder (8JR6566HZ6)' -vvvv --timestamp --options runtime --prefix 'edu.colorado.cires.pace.cli.' --keychain "$1" --force org/sqlite/native/Mac/x86_64/libsqlitejdbc.dylib
 
-echo Repacking dylib files
+echo Repacking cli dylib files
 jar -uf ./jars/pace-cli-0.1.1-SNAPSHOT.jar org/sqlite/native/Mac/aarch64/libsqlitejdbc.dylib
 jar -uf ./jars/pace-cli-0.1.1-SNAPSHOT.jar org/sqlite/native/Mac/x86_64/libsqlitejdbc.dylib
+
+echo Extracting gui dylib files
+jar -xf ./jars/pace-gui-0.1.1-SNAPSHOT.jar org/sqlite/native/Mac/aarch64/libsqlitejdbc.dylib
+jar -xf ./jars/pace-gui-0.1.1-SNAPSHOT.jar org/sqlite/native/Mac/x86_64/libsqlitejdbc.dylib
+
+echo Signing gui dylib files
+/usr/bin/codesign -s 'Developer ID Application: University of Colorado Boulder (8JR6566HZ6)' -vvvv --timestamp --options runtime --prefix 'edu.colorado.cires.pace.gui.' --keychain "$1" --force org/sqlite/native/Mac/aarch64/libsqlitejdbc.dylib
+/usr/bin/codesign -s 'Developer ID Application: University of Colorado Boulder (8JR6566HZ6)' -vvvv --timestamp --options runtime --prefix 'edu.colorado.cires.pace.gui.' --keychain "$1" --force org/sqlite/native/Mac/x86_64/libsqlitejdbc.dylib
+
+echo Repacking gui dylib files
+jar -uf ./jars/pace-gui-0.1.1-SNAPSHOT.jar org/sqlite/native/Mac/aarch64/libsqlitejdbc.dylib
+jar -uf ./jars/pace-gui-0.1.1-SNAPSHOT.jar org/sqlite/native/Mac/x86_64/libsqlitejdbc.dylib
