b3499b919814be5c47c3068c09178d6f49960bd4
Quincy Cantu <quincyscantu@gmail.com>
2024-09-09 09:00:49 -0600

    Signing correct gui files

--- build-resources/mac-signing-gui.sh
+++ build-resources/mac-signing-gui.sh
@@ -2,10 +2,10 @@ set -ex
 
 ls jars
 
-#echo Extracting gui dylib files
-#jar -xf ./jars/pace-gui-0.1.1-SNAPSHOT.jar org/sqlite/native/Mac/aarch64/libsqlitejdbc.dylib
-#jar -xf ./jars/pace-gui-0.1.1-SNAPSHOT.jar org/sqlite/native/Mac/x86_64/libsqlitejdbc.dylib
-#
+echo Extracting gui dylib files
+jar -xf ./jars/pace-gui-0.1.1-SNAPSHOT.jar com/formdev/flatlaf/natives/libflatlaf-macos-arm64.dylib
+jar -xf ./jars/pace-gui-0.1.1-SNAPSHOT.jar com/formdev/flatlaf/natives/libflatlaf-macos-x86_64.dylib
+
 #echo Signing gui dylib files
 #/usr/bin/codesign -s 'Developer ID Application: University of Colorado Boulder (8JR6566HZ6)' -vvvv --timestamp --options runtime --prefix 'edu.colorado.cires.pace.gui.' --keychain "$1" --force org/sqlite/native/Mac/aarch64/libsqlitejdbc.dylib
 #/usr/bin/codesign -s 'Developer ID Application: University of Colorado Boulder (8JR6566HZ6)' -vvvv --timestamp --options runtime --prefix 'edu.colorado.cires.pace.gui.' --keychain "$1" --force org/sqlite/native/Mac/x86_64/libsqlitejdbc.dylib
