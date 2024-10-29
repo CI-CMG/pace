33490b278ec2184e2316f24ab5a0257f45dd2a75
Quincy Cantu <quincyscantu@gmail.com>
2024-10-29 15:18:11 -0600

    Made mac signing generalize for version

--- build-resources/mac-signing-gui.sh
+++ build-resources/mac-signing-gui.sh
@@ -3,13 +3,13 @@ set -ex
 ls jars
 
 echo Extracting gui dylib files
-jar -xf ./jars/pace-gui-0.1.1-SNAPSHOT.jar com/formdev/flatlaf/natives/libflatlaf-macos-arm64.dylib
-jar -xf ./jars/pace-gui-0.1.1-SNAPSHOT.jar com/formdev/flatlaf/natives/libflatlaf-macos-x86_64.dylib
+jar -xf $2 com/formdev/flatlaf/natives/libflatlaf-macos-arm64.dylib
+jar -xf $2 com/formdev/flatlaf/natives/libflatlaf-macos-x86_64.dylib
 
 echo Signing gui dylib files
 /usr/bin/codesign -s 'Developer ID Application: University of Colorado Boulder (8JR6566HZ6)' -vvvv --timestamp --options runtime --prefix 'edu.colorado.cires.pace.gui.' --keychain "$1" --force com/formdev/flatlaf/natives/libflatlaf-macos-arm64.dylib
 /usr/bin/codesign -s 'Developer ID Application: University of Colorado Boulder (8JR6566HZ6)' -vvvv --timestamp --options runtime --prefix 'edu.colorado.cires.pace.gui.' --keychain "$1" --force com/formdev/flatlaf/natives/libflatlaf-macos-x86_64.dylib
 
 echo Repacking gui dylib files
-jar -uf ./jars/pace-gui-0.1.1-SNAPSHOT.jar com/formdev/flatlaf/natives/libflatlaf-macos-arm64.dylib
-jar -uf ./jars/pace-gui-0.1.1-SNAPSHOT.jar com/formdev/flatlaf/natives/libflatlaf-macos-x86_64.dylib
+jar -uf $2 com/formdev/flatlaf/natives/libflatlaf-macos-arm64.dylib
+jar -uf $2 com/formdev/flatlaf/natives/libflatlaf-macos-x86_64.dylib
--- build-resources/mac-signing.sh
+++ build-resources/mac-signing.sh
@@ -1,13 +1,13 @@
 set -ex
 
 echo Extracting cli dylib files
-jar -xf ./jars/pace-cli-0.1.1-SNAPSHOT.jar org/sqlite/native/Mac/aarch64/libsqlitejdbc.dylib
-jar -xf ./jars/pace-cli-0.1.1-SNAPSHOT.jar org/sqlite/native/Mac/x86_64/libsqlitejdbc.dylib
+jar -xf $2 org/sqlite/native/Mac/aarch64/libsqlitejdbc.dylib
+jar -xf $2 org/sqlite/native/Mac/x86_64/libsqlitejdbc.dylib
 
 echo Signing cli dylib files
 /usr/bin/codesign -s 'Developer ID Application: University of Colorado Boulder (8JR6566HZ6)' -vvvv --timestamp --options runtime --prefix 'edu.colorado.cires.pace.cli.' --keychain "$1" --force org/sqlite/native/Mac/aarch64/libsqlitejdbc.dylib
 /usr/bin/codesign -s 'Developer ID Application: University of Colorado Boulder (8JR6566HZ6)' -vvvv --timestamp --options runtime --prefix 'edu.colorado.cires.pace.cli.' --keychain "$1" --force org/sqlite/native/Mac/x86_64/libsqlitejdbc.dylib
 
 echo Repacking cli dylib files
-jar -uf ./jars/pace-cli-0.1.1-SNAPSHOT.jar org/sqlite/native/Mac/aarch64/libsqlitejdbc.dylib
-jar -uf ./jars/pace-cli-0.1.1-SNAPSHOT.jar org/sqlite/native/Mac/x86_64/libsqlitejdbc.dylib
+jar -uf $2 org/sqlite/native/Mac/aarch64/libsqlitejdbc.dylib
+jar -uf $2 org/sqlite/native/Mac/x86_64/libsqlitejdbc.dylib
